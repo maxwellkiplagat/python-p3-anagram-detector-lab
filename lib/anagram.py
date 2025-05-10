d79eefa51a7b73255862fac05d28551c1610eee4
maxwellkiplagat <maxwellkiplagat0@gmail.com>
2025-05-10 09:50:38 +0300

    finsihed the assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def is_anagram(self, match):
+        return sorted(self.word) == sorted(match.lower())
+
+    def match(self, words):  # <- this is the required method
+        return [word for word in words if self.is_anagram(word)]
